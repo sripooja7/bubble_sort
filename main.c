19e223f7e218a6761dedc956be8f9b6867e0ebd8
sripooja7 <33750194+sripooja7@users.noreply.github.com>
2019-05-29 18:44:07 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,36 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int n,i,j,temp=0;
+    printf("Enter the size of the array\n");
+    scanf("%d",&n);
+        int a[n];
+    printf("Enter the elements into the array\n");
+    for(i=0;i<n;i++)
+    scanf("%d",&a[i]);
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<(n-i-1);j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+            
+        }
+    }
+    for(i=0;i<n;i++)
+    printf("%d ",a[i]);
+    return 0;
+}
